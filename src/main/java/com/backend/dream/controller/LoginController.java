c24eccfce8e9ae7127636502d2a80bf8f9f4ebd4
KimVanCuong <vancuo1702asd@gmail.com>
2023-10-04 16:43:18 +0700

    add controller error

--- src/main/java/com/backend/dream/controller/LoginController.java
+++ src/main/java/com/backend/dream/controller/LoginController.java
@@ -4,6 +4,7 @@ import com.backend.dream.entity.Account;
 import com.backend.dream.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -45,7 +46,8 @@ public class LoginController {
         }
     }
     @RequestMapping("/auth/error")
-    public String error(){
-        return "/form";
+    public String error(Model model){
+        model.addAttribute("message","Username and password incorrect");
+        return "loginForm";
     }
 }
