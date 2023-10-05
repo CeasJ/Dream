9851ef8e12370ff9dfb49dda556de5e68cfa9a09
toanvu <toanvu2311@gmail.com>
2023-10-05 22:35:41 +0700

    resole config

--- src/main/java/com/backend/dream/config/SecurityConfig.java
+++ src/main/java/com/backend/dream/config/SecurityConfig.java
@@ -1,63 +1,63 @@
-//package com.backend.dream.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationProvider;
-//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//@EnableMethodSecurity
-//public class SecurityConfig {
-//    @Bean
-//    public UserDetailsService userDetailsServices(){
-//        return new UserDetailService();
-//    }
-//    @Bean
-//    public PasswordEncoder passwordEncoder(){
-//        return new BCryptPasswordEncoder();
-//    }
-//    @Bean
-//    public WebSecurityCustomizer webSecurityCustomizer(){
-//        return (web) -> web.ignoring().requestMatchers("/ignore1","ignore2");
-//    }
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-//        return http.csrf(csrf->csrf.disable())
-//                .authorizeHttpRequests(auth->auth
-//                        .requestMatchers("/home").permitAll()
-//                        .requestMatchers("/product").authenticated()
-//                        .requestMatchers("/admin").hasAuthority("ADMIN")
-//                        .requestMatchers("/staff").hasAuthority("STAFF")
-//                        .anyRequest().permitAll()
-//                )
-////                .formLogin(login->login
-////                        .loginPage("/form")
-////                        .loginProcessingUrl("/login")
-////                        .defaultSuccessUrl("/product")
-////                        .failureUrl("/login/error")
-////                )
-////                .logout(logout->logout
-////                        .logoutUrl("/auth/logout")
-////                        .logoutSuccessUrl("/home")
-////                )
-////                .rememberMe(remember->remember.tokenValiditySeconds(86400))
-//                .build();
-//    }
-//    @Bean
-//    public AuthenticationProvider authenticationProvider(){
-//        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-//        authenticationProvider.setUserDetailsService(userDetailsServices());
-//        authenticationProvider.setPasswordEncoder(passwordEncoder());
-//        return authenticationProvider;
-//    }
-//
-//}
+package com.backend.dream.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsServices(){
+        return new UserDetailService();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return (web) -> web.ignoring().requestMatchers("/ignore1","ignore2");
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http.csrf(csrf->csrf.disable())
+                .authorizeHttpRequests(auth->auth
+                        .requestMatchers("/home").permitAll()
+                        .requestMatchers("/product").authenticated()
+                        .requestMatchers("/admin").hasAuthority("ADMIN")
+                        .requestMatchers("/staff").hasAuthority("STAFF")
+                        .anyRequest().permitAll()
+                )
+                .formLogin(login->login
+                        .loginPage("/form")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/product")
+                        .failureUrl("/login/error")
+                )
+                .logout(logout->logout
+                        .logoutUrl("/auth/logout")
+                        .logoutSuccessUrl("/home")
+                )
+                .rememberMe(remember->remember.tokenValiditySeconds(86400))
+                .build();
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsServices());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+}
