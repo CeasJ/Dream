1a7cfe470535dcfcdf4e3694934730290972c56a
KimVanCuong <vancuo1702asd@gmail.com>
2023-10-13 12:15:22 +0700

    fix security branch kiet

--- src/main/java/com/backend/dream/config/SecurityConfig.java
+++ src/main/java/com/backend/dream/config/SecurityConfig.java
@@ -1,4 +1,3 @@
-<<<<<<<<<Temporary merge branch 1
 package com.backend.dream.config;
 
 import org.springframework.context.annotation.Bean;
@@ -62,77 +61,5 @@ public class SecurityConfig {
         return authenticationProvider;
     }
 
-}=========
-// package com.backend.dream.config;
-//
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.authentication.AuthenticationProvider;
-// import
-// org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-// import
-// org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-// import
-// org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import
-// org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import
-// org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-// import org.springframework.security.core.userdetails.UserDetailsService;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// import org.springframework.security.crypto.password.PasswordEncoder;
-// import org.springframework.security.web.SecurityFilterChain;
-//
-// @Configuration
-// @EnableWebSecurity
-// @EnableMethodSecurity
-// public class SecurityConfig {
-// @Bean
-// public UserDetailsService userDetailsServices(){
-// return new UserDetailService();
-// }
-// @Bean
-// public PasswordEncoder passwordEncoder(){
-// return new BCryptPasswordEncoder();
-// }
-// @Bean
-// public WebSecurityCustomizer webSecurityCustomizer(){
-// return (web) -> web.ignoring().requestMatchers("/ignore1","ignore2");
-// }
-// @Bean
-// public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-// return http.csrf(csrf->csrf.disable())
-// .authorizeHttpRequests(auth->auth
-// .requestMatchers("/home").permitAll()
-// .requestMatchers("/products/index").authenticated()
-// .requestMatchers("/admin/index").hasAuthority("ADMIN")
-// .requestMatchers("/staff/index").hasAuthority("STAFF")
-// .anyRequest().permitAll()
-// )
-// .formLogin(login->login
-// .loginPage("/form")
-// .loginProcessingUrl("/login")
-// .defaultSuccessUrl("/products/index")
-// .failureUrl("/auth/error")
-// )
-// .logout(logout->logout
-// .logoutUrl("/auth/logout")
-// .logoutSuccessUrl("/form")
-// )
-//// .rememberMe(remember->remember.tokenValiditySeconds(86400))
-// .build();
-// }
-// @Bean
-// public AuthenticationProvider authenticationProvider(){
-// DaoAuthenticationProvider authenticationProvider = new
-// DaoAuthenticationProvider();
-// authenticationProvider.setUserDetailsService(userDetailsServices());
-// authenticationProvider.setPasswordEncoder(passwordEncoder());
-// return authenticationProvider;
-// }
-//
-// }
->>>>>>>>>
+}
 
-Temporary merge branch
-2
