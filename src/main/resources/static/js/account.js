a721f6234e77073fdd92b34bf8b3ffc9f1890cdc
toanvu <toanvu2311@gmail.com>
2023-11-07 22:50:30 +0700

    add address

--- src/main/resources/static/js/account.js
+++ src/main/resources/static/js/account.js
@@ -1,7 +1,77 @@
 const app = angular.module("profile", []);
 app.controller("profile_ctrl", function ($scope, $http) {
   $scope.account = {};
+  $scope.provinces = [];
+  $scope.districts = [];
+  $scope.wards = [];
+  $scope.selectedProvince = "";
+  $scope.selectedDistrict = "";
+  $scope.selectedWard = "";
+  $scope.result = "";
 
+  $http
+    .get("https://provinces.open-api.vn/api/?depth=1")
+    .then(function (response) {
+      $scope.provinces = response.data;
+    });
+
+  $scope.getDistricts = function () {
+    if ($scope.selectedProvince) {
+      $http
+        .get(
+          "https://provinces.open-api.vn/api/p/" +
+          $scope.selectedProvince +
+          "?depth=2"
+        )
+        .then(function (response) {
+          $scope.districts = response.data.districts;
+        });
+    }
+  };
+
+  $scope.getWards = function () {
+    if ($scope.selectedDistrict) {
+      $http
+        .get(
+          "https://provinces.open-api.vn/api/d/" +
+          $scope.selectedDistrict +
+          "?depth=2"
+        )
+        .then(function (response) {
+          $scope.wards = response.data.wards;
+        });
+    }
+  };
+
+  $scope.getSelectedProvinces = function (code) {
+    code = parseInt(code);
+    for (let i = 0; i < $scope.provinces.length; i++) {
+      if ($scope.provinces[i].code === code) {
+        return $scope.provinces[i].name;
+      }
+    }
+    return "";
+  };
+
+  $scope.getSelectedDistricts = function (code) {
+    code = parseInt(code);
+    for (let i = 0; i < $scope.districts.length; i++) {
+      if ($scope.districts[i].code === code) {
+        return $scope.districts[i].name;
+      }
+    }
+    return "";
+  };
+
+  $scope.getSelectedWards = function (code) {
+    code = parseInt(code);
+    for (let i = 0; i < $scope.wards.length; i++) {
+      if ($scope.wards[i].code === code) {
+        return $scope.wards[i].name;
+      }
+    }
+    return "";
+  };
   let idString = $("#id_account").text().trim();
   let id_account = parseInt(idString);
   $scope.initialize = function () {
@@ -67,4 +137,13 @@ app.controller("profile_ctrl", function ($scope, $http) {
         })
         .catch((err) => {});
     };
+      $scope.printResult = function () {
+        if ($scope.selectedProvince && $scope.selectedDistrict && $scope.selectedWard) {
+          $scope.account.address =
+            $scope.number + "," +
+            $scope.getSelectedWards($scope.selectedWard) + "," +
+            $scope.getSelectedDistricts($scope.selectedDistrict) + "," +
+            $scope.getSelectedProvinces($scope.selectedProvince);
+        }
+      };
 });
