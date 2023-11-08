77d9f91bfc4c1ca1f65d84a5b7440f2f811d5540
Manh Thanh <tranmanhthanh15@gmail.com>
2023-11-08 12:47:51 +0700

    update admin orders

--- src/main/resources/static/js/account.js
+++ src/main/resources/static/js/account.js
@@ -1,3 +1,71 @@
+(function ($) {
+  "use strict";
+
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner();
+
+  new WOW().init();
+
+  // Sticky Navbar
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".sticky-top").addClass("shadow-sm").css("top", "0px");
+    } else {
+      $(".sticky-top").removeClass("shadow-sm").css("top", "-150px");
+    }
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+
+  // Show .cart-0 if cartCount is less than or equal to 0, otherwise show .cart-1
+  var cartCount = parseInt($("#cartCount").text());
+
+  $(document).ready(function () {
+    $("#applyDiscountBtn").click(function () {
+      var toast = new bootstrap.Toast(document.getElementById("successToast"));
+      toast.show();
+    });
+  });
+
+  $(document).ready(function () {
+    $(".theme-mode input").change(function () {
+      if (this.checked) {
+        // Chuyển sang dark mode
+        $("body").removeClass("light-mode").addClass("dark-mode");
+      } else {
+        // Chuyển sang light mode
+        $("body").removeClass("dark-mode").addClass("light-mode");
+      }
+    });
+  });
+
+  // Lấy giá trị của cartCount và chuyển đổi thành số nguyên
+  var cartCountValue = parseInt($('#cartCount').text());
+
+  // Kiểm tra nếu giá trị cartCount nhỏ hơn hoặc bằng 0, ẩn phần tử span
+  if (cartCountValue <= 0) {
+    $('#cartCount').hide();
+  } else {
+    $('#cartCount').show();
+  }
+
+
+})(jQuery);
+
 const app = angular.module('profile', []);
 app.controller('profile_ctrl', function ($scope, $http, $rootScope) {
   $scope.account = {};
@@ -148,6 +216,7 @@ app.controller('profile_ctrl', function ($scope, $http, $rootScope) {
         $scope.getSelectedProvinces($scope.selectedProvince);
     }
   };
+
 });
 
 
