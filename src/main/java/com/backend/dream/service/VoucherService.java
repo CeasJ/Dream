a4cc705df90ed11d7a29e521ed5a77282aea8d6d
CeasJ <kietlai667@gmail.com>
2023-11-28 21:18:06 +0700

    delete and side function voucher management

--- src/main/java/com/backend/dream/service/VoucherService.java
+++ src/main/java/com/backend/dream/service/VoucherService.java
@@ -1,9 +1,21 @@
 package com.backend.dream.service;
 
 import com.backend.dream.dto.VoucherDTO;
+import com.backend.dream.dto.VoucherStatusDTO;
 
 import java.util.List;
 
 public interface VoucherService {
     List<VoucherDTO> getApplicableVouchers(String username);
+
+    List<VoucherDTO> getAllVouchers();
+
+    // Display the voucher status in combobox
+    List<VoucherStatusDTO> getAllVoucherStatus();
+
+    List<VoucherDTO> getVouchersByStatusId(Long statusId);
+
+    List<VoucherDTO> searchVouchersByName(String name);
+
+    void delete(Long id);
 }
--- src/main/java/com/backend/dream/service/imp/VoucherServiceImp.java
+++ src/main/java/com/backend/dream/service/imp/VoucherServiceImp.java
@@ -1,9 +1,13 @@
 package com.backend.dream.service.imp;
 
 import com.backend.dream.dto.VoucherDTO;
+import com.backend.dream.dto.VoucherStatusDTO;
 import com.backend.dream.entity.Voucher;
+import com.backend.dream.entity.VoucherStatus;
 import com.backend.dream.mapper.VoucherMapper;
+import com.backend.dream.mapper.VoucherStatusMapper;
 import com.backend.dream.repository.VoucherRepository;
+import com.backend.dream.repository.VoucherStatusRepository;
 import com.backend.dream.service.AccountService;
 import com.backend.dream.service.VoucherService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +28,9 @@ public class VoucherServiceImp implements VoucherService {
     @Autowired
     AccountService accountService;
 
+    @Autowired
+    VoucherStatusRepository voucherStatusRepository;
+
     @Override
     public List<VoucherDTO> getApplicableVouchers(String username) {
         Long userId = accountService.findIDByUsername(username);
@@ -32,5 +39,42 @@ public class VoucherServiceImp implements VoucherService {
         return applicableVouchers.stream()
                 .map(voucherMapper::voucherToVoucherDTO)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<VoucherDTO> getAllVouchers() {
+        List<Voucher> vouchers = voucherRepository.findAll();
+        return vouchers.stream()
+                .map(VoucherMapper.INSTANCE::voucherToVoucherDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<VoucherStatusDTO> getAllVoucherStatus() {
+        List<VoucherStatus> voucherStatusList = voucherStatusRepository.findAll();
+        return voucherStatusList.stream()
+                .map(VoucherStatusMapper.INSTANCE::voucherStatusToVoucherStatusDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<VoucherDTO> getVouchersByStatusId(Long statusId) {
+        List<Voucher> vouchers = voucherRepository.findByStatusId(statusId);
+        return vouchers.stream()
+                .map(voucherMapper::voucherToVoucherDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<VoucherDTO> searchVouchersByName(String name) {
+        List<Voucher> vouchers = voucherRepository.searchByName(name);
+        return vouchers.stream()
+                .map(voucherMapper::voucherToVoucherDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void delete(Long id) {
+        voucherRepository.deleteById(id);
     }
 }
