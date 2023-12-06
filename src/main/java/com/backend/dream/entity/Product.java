a854fdd9416ca581d19013861c7917e59b0717ed
CeasJ <kietlai667@gmail.com>
2023-12-06 17:29:13 +0700

    Update tests case

--- src/main/java/com/backend/dream/entity/Product.java
+++ src/main/java/com/backend/dream/entity/Product.java
@@ -2,10 +2,7 @@ package com.backend.dream.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -17,6 +14,7 @@ import java.util.List;
 @Setter
 @Entity
 @Table
+@Builder
 public class Product implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/test/java/com/test/dream/SearchingTest.java
+++ src/test/java/com/test/dream/SearchingTest.java
@@ -1,64 +1,151 @@
-//package com.test.dream;
-//
-//import com.backend.dream.dto.ProductDTO;
-//import com.backend.dream.entity.Product;
-//import com.backend.dream.repository.ProductRepository;
-//import com.backend.dream.service.ProductService;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.PageImpl;
-//import org.springframework.data.domain.Pageable;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static org.mockito.Mockito.*;
-//
-//public class SearchingTest {
-//
-//    @Mock
-//    private ProductRepository productRepository;
-//
-//    @InjectMocks
-//    private ProductService productService;
-//
-//    @BeforeEach
-//    public void setup() {
-//        MockitoAnnotations.initMocks(this);
-//    }
-//
-//    @Test
-//    public void testSearchProductByName() {
-//        List<Product> mockProductList = new ArrayList<>();
-//        Product product1 = new Product();
-//        product1.setId(1L);
-//        product1.setName("Bạc xỉu");
-//        // Thêm các thuộc tính khác của sản phẩm nếu cần
-//        mockProductList.add(product1);
-//
-//        // Tạo Pageable object
-//        Pageable pageable = Pageable.unpaged();
-//
-//        // Giả định kết quả trả về từ repository khi tìm kiếm theo tên "Bạc xỉu"
-//        Page<Product> mockProductPage = new PageImpl<>(mockProductList);
-//
-//        // Mock behavior của productRepository khi gọi method findByNameContainingIgnoreCase
-//        when(productRepository.findByNameContainingIgnoreCase(eq("Bạc xỉu"), any(Pageable.class)))
-//                .thenReturn(mockProductPage);
-//
-//        // Thực hiện chức năng tìm kiếm sản phẩm theo tên
-//        Page<ProductDTO> result = productService.findByNamePaged("Bạc xỉu", pageable);
-//
-//        // Kiểm tra kết quả trả về có chứa sản phẩm "Bạc xỉu" hay không
-//        List<ProductDTO> productList = result.getContent();
-//        boolean productFound = productList.stream().anyMatch(productDTO -> "Bạc xỉu".equals(productDTO.getName()));
-//
-//        // Asserts
-//        assert(productFound); // Kiểm tra sản phẩm "Bạc xỉu" có trong danh sách sản phẩm kết quả hay không
-//        verify(productRepository, times(1)).findByNameContainingIgnoreCase(eq("Bạc xỉu"), any(Pageable.class)); // Kiểm tra xem repository đã được gọi với đúng tham số không
-//    }
-//}
+package com.test.dream;
+
+import com.backend.dream.entity.Product;
+import com.backend.dream.repository.ProductRepository;
+import com.backend.dream.service.imp.ProductServiceImp;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class SearchingTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @InjectMocks
+    private ProductServiceImp productService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testSearchByName() {
+        List<Product> mockProductList = new ArrayList<>();
+        Product product1 = Product.builder()
+                .id(1L)
+                .name("Bạc xỉu")
+                .price(10.0)
+                .image("image1.jpg")
+                .describe("Description for Bạc xỉu")
+                .createDate(new Date())
+                .active(true)
+                .build();
+
+        Product product2 = Product.builder()
+                .id(2L)
+                .name("Hồng trà")
+                .price(8.5)
+                .image("image2.jpg")
+                .describe("Description for Hồng trà")
+                .createDate(new Date())
+                .active(true)
+                .build();
+
+        mockProductList.add(product1);
+        mockProductList.add(product2);
+
+        String searchProductName = "Bạc xỉu";
+        boolean found = false;
+
+        for (Product product : mockProductList) {
+            if (product.getName().equalsIgnoreCase(searchProductName)) {
+                found = true;
+                break;
+            }
+        }
+
+        assertEquals(true, found, "Product with name '" + searchProductName + "' not found in the list.");
+    }
+
+    @Test
+    public void testSearchByIncorrectName() {
+        List<Product> mockProductList = new ArrayList<>();
+        Product product1 = Product.builder()
+                .id(1L)
+                .name("Bạc xỉu")
+                .price(10.0)
+                .image("image1.jpg")
+                .describe("Description for Bạc xỉu")
+                .createDate(new Date())
+                .active(true)
+                .build();
+
+        Product product2 = Product.builder()
+                .id(2L)
+                .name("Hồng trà")
+                .price(8.5)
+                .image("image2.jpg")
+                .describe("Description for Hồng trà")
+                .createDate(new Date())
+                .active(true)
+                .build();
+
+        mockProductList.add(product1);
+        mockProductList.add(product2);
+
+        String searchProductName = "Cơm";
+        boolean found = false;
+
+        for (Product product : mockProductList) {
+            if (product.getName().equalsIgnoreCase(searchProductName)) {
+                found = true;
+                break;
+            }
+        }
+
+        assertEquals(false, found, "Product with name '" + searchProductName + "' should not be found in the list.");
+    }
+
+    @Test
+    public void testSearchByTwoSpaces() {
+        List<Product> mockProductList = new ArrayList<>();
+        Product product1 = Product.builder()
+                .id(1L)
+                .name("Bạc xỉu")
+                .price(10.0)
+                .image("image1.jpg")
+                .describe("Description for Bạc xỉu")
+                .createDate(new Date())
+                .active(true)
+                .build();
+
+        Product product2 = Product.builder()
+                .id(2L)
+                .name("Hồng trà")
+                .price(8.5)
+                .image("image2.jpg")
+                .describe("Description for Hồng trà")
+                .createDate(new Date())
+                .active(true)
+                .build();
+
+        mockProductList.add(product1);
+        mockProductList.add(product2);
+
+        // Sử dụng tên sản phẩm với hai khoảng trắng
+        String searchProductName = "  "; // Hai khoảng trắng
+        boolean found = false;
+
+        for (Product product : mockProductList) {
+            if (product.getName().equalsIgnoreCase(searchProductName.trim())) {
+                found = true;
+                break;
+            }
+        }
+
+        assertEquals(false, found, "Product with two spaces should not be found in the list.");
+    }
+
+}
